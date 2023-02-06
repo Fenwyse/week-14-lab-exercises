a7588bbf3296d29343a10209c830e3113a3b5f73
Julia Redfern <Julia.Redfern@warwick.ac.uk>
2023-02-06 11:41:11 +0000

    Fix index.js again

--- mashup/index.js
+++ mashup/index.js
@@ -1,13 +1,14 @@
-const myStatement = false;
+myStatement = false;
 console.log("Hello this is mashup js");
 
 if (myStatement = false){
   console.log("statement is false");
+  console.log(myStatement)
 }
 
 {
   console.log("This is mashup" + "strings");
-  if (true == true) {
-    console.log("True is equal to true");
+  if (myStatement == true) {
+    console.log("statement is equal to true");
    }
 }
